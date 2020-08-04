1050800edc80208c87af2eee28700d6414f9ce23
Sudarshan Giri <41857483+SudarshanGiri@users.noreply.github.com>
2020-08-04 14:34:19 +0545

    added active nav

--- js/main.js
+++ js/main.js
@@ -3,3 +3,5 @@ document.querySelector('.burger').addEventListener('click',function(){
     document.querySelector('.nav-links').classList.toggle('nav-links-show');
     document.querySelector('.burger').classList.toggle('burger-animate');
 })
+
+//one page nav
